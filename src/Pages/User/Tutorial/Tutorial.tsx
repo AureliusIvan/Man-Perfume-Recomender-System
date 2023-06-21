1c5670a628e84d9707774df226c993e2dd0f0844
maecyntha <maecyntha.tan@gmail.com>
2023-06-21 17:57:50 +0700

    update tutorial style

--- src/Pages/User/Tutorial/Tutorial.tsx
+++ src/Pages/User/Tutorial/Tutorial.tsx
@@ -1,97 +1,91 @@
-import { Paragraf } from "../../../Component/StyledComponent/Typography/CustomTypography"
-import { Grid } from "@mui/material"
-import { styled } from "@material-ui/styles"
-import { TutorialData } from "./TutorialData"
+import { Paragraf } from "../../../Component/StyledComponent/Typography/CustomTypography";
+import { Grid } from "@mui/material";
+import { styled } from "@material-ui/styles";
+import { TutorialData } from "./TutorialData";
 
-import style from "./Tutorial.module.scss"
+import style from "./Tutorial.module.scss";
 
-import useMediaQuery from "@mui/material/useMediaQuery"
-import { motion } from "framer-motion"
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { motion } from "framer-motion";
 
-import { useTranslation } from "react-i18next"
-
-const GridItem = styled(Grid)(() => ({
-    // padding: '20px',
-    // width: '90%',
-}));
-
-
-interface defaulprops {
-    text: string,
-    title: string,
-    img: string,
-}
-
-function Card(props: defaulprops) {
-    const Mobile = useMediaQuery('(max-width:600px)');
-
-
-    return (<motion.div
-    >
-        <Grid container
-            className={style.card}
-        >
-            {Mobile ?
-                <>
-                    <Grid item xs={12} sm={9} md={9} lg={10}
-                        sx={{
-                            display: 'flex',
-                            alignItems: 'center',
-                            justifyContent: 'center',
-                            textAlign: 'center',
-                        }}
-                    >
-                        <h3 className={style.h3}>
-                            {props.title}
-                        </h3>
-                    </Grid>
-                    <br />
-                    <p className={style.p}>
-                        {props.text}
-                    </p>
-                </>
-                :
-                <>
-                    <Grid item sm={9} md={9} lg={12}>
-                        <h3 className={style.h3}>
-                            {props.title}
-                        </h3>
-                        <p className={style.p}>
-                            {props.text}
-                        </p>
-                    </Grid>
-                </>
-            }
-        </Grid>
-    </motion.div>
-    )
-}
+import { useTranslation } from "react-i18next";
+import { Flex } from "@/Component/StyledComponent/CustomBox/CustomBox";
+import { useEffect, useState } from "react";
+import { CustomButton } from "@/Component/StyledComponent/CustomButton/CustomButton";
+import { Button } from "@material-ui/core";
+import { max } from "lodash";
 
 export default function Tutorial() {
     const { t } = useTranslation();
-    const Mobile = useMediaQuery('(max-width:600px)');
-    
-    return (<>
-        <Paragraf title={t("homeTitle2")}>
-            <i>{t("homeDesc2")}</i>
-        </Paragraf>
-        <Grid container sx={{
-            width: Mobile ? '90%' : '50%',
-            padding: '10px',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-        }}>
-            {
-                TutorialData.map((item, index) => {
-                    return (
-                        <GridItem key={index} item xs={12}>
-                            <Card img={item.image} title={item.title} text={item.description} />
-                        </GridItem>
-                    )
-                })
-            }
-        </Grid >
-    </>
-    )
+    const [clicked, setClicked] = useState(0);
+
+    const handleClick = (id: any) => {
+        setClicked(id);
+    };
+
+    const mobile = useMediaQuery<boolean>("(max-width:900px)");
+
+    return (
+        <>
+            <Paragraf title={t("homeTitle2")}>
+                <i>{t("homeDesc2")}</i>
+            </Paragraf>
+            <div className={style.container}>
+                {!mobile ? (
+                    <Description
+                        title={t("step") + " " + TutorialData[clicked].id}
+                        text={t(`stepDesc${TutorialData[clicked].id}`)}
+                    />
+                ) : null}
+                <div className={style.content}>
+                    <div className={style.bulletGrp}>
+                        {TutorialData.map((id, i) => (
+                            <Button
+                                onClick={() => {
+                                    handleClick(i);
+                                }}
+                                className={
+                                    i === clicked
+                                        ? `${style.active}`
+                                        : `${style.btn}`
+                                }
+                            >
+                                <div className={style.bullet}>{i + 1}</div>
+                            </Button>
+                        ))}
+                    </div>
+                    {mobile ? (
+                        <Description
+                            title={t("step") + " " + TutorialData[clicked].id}
+                            text={t(`stepDesc${TutorialData[clicked].id}`)}
+                        />
+                    ) : null}
+                    <Card
+                        img={
+                            mobile
+                                ? TutorialData[clicked].imageM
+                                : TutorialData[clicked].image
+                        }
+                    />
+                </div>
+            </div>
+        </>
+    );
+}
+
+function Card(props: any) {
+    return (
+        <div className={style.card}>
+            <img className={style.img} src={props.img} />
+        </div>
+    );
+}
+
+function Description(props: any) {
+    return (
+        <div className={style.desc}>
+            <h3 className={style.h3}>{props.title}</h3>
+            <p className={style.p}>{props.text}</p>
+        </div>
+    );
 }
