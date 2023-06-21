c151baacf90531d4d5b1505f8fca602f1bc30bef
maecyntha <maecyntha.tan@gmail.com>
2023-06-21 17:57:11 +0700

    insert tutorial images

--- src/Pages/User/Tutorial/TutorialData.ts
+++ src/Pages/User/Tutorial/TutorialData.ts
@@ -1,27 +1,32 @@
-import Comp from "../../../Assets/Image/Comp.png";
-// import Learn from "../../../Assets/Image/Learn.png";
-import Write from "../../../Assets/Image/Write.png";
-import Monit from "../../../Assets/Image/Monit.png";
+import step1 from "@/Assets/Image/tutorial/step1.png";
+import step2 from "@/Assets/Image/tutorial/step2.png";
+import step3 from "@/Assets/Image/tutorial/step3.png";
+import step4 from "@/Assets/Image/tutorial/step4.png";
 
-import { useTranslation } from "react-i18next"
+import step1M from "@/Assets/Image/tutorial/mobile/step1-m.png";
+import step2M from "@/Assets/Image/tutorial/mobile/step2-m.png";
+import step3M from "@/Assets/Image/tutorial/mobile/step3-m.png";
+import step4M from "@/Assets/Image/tutorial/mobile/step4-m.png";
 
 export const TutorialData = [
     {
         id: 1,
-        title: 'Tekanlah',
-        description: 'Klik tombol "Mulai Menentukan Parfum" di halaman pertama. Anda akan langsung diarahkan ke halaman penentuan prioritas',
-        image: Comp,
+        image: step1,
+        imageM: step1M,
     },
     {
         id: 2,
-        title: 'Pilihlah',
-        description: 'Pilihlah prioritas dari 2 kriteria berbeda yang dibandingkan sesuai preferensi anda sebagai pria. Masing-Masing skala bernilai minimal 1 dan maksimal 3',
-        image: Write,
+        image: step2,
+        imageM: step2M,
     },
     {
         id: 3,
-        title: 'Submit',
-        description: 'Setelah anda selesai, maka klik submit untuk melihat hasil dari penelitian kami!',
-        image: Monit,
+        image: step3,
+        imageM: step3M,
+    },
+    {
+        id: 4,
+        image: step4,
+        imageM: step4M,
     },
 ] 
